677ea016d0e2bc188aef301f28b9da4ac004e005
Richard Fric <richard.fric@decent.ch>
2019-06-10 11:25:18 +0200

    Prepare 1.4.0 release

--- dcore.rb
+++ dcore.rb
@@ -1,14 +1,14 @@
 class Dcore < Formula
   desc "Fast, powerful and cost-efficient blockchain"
   homepage "https://decent.ch/dcore"
-  url "https://github.com/DECENTfoundation/DECENT-Network.git", :using => :git, :tag => "1.4.0", :revision => "6c6b0f7241e1cb3cec68fd60a41c4185dbc64f4e"
+  url "https://github.com/DECENTfoundation/DECENT-Network.git", :using => :git, :tag => "1.4.0", :revision => "383ecb36e5f7d6d330824b56661549e890122321"
   head "https://github.com/DECENTfoundation/DECENT-Network.git", :branch => "develop", :shallow => false
 
   bottle do
     root_url "https://dl.bintray.com/decentfoundation/homebrew"
     cellar :any_skip_relocation
-    sha256 "d7671829bf1d5af2ea5d40d230a50ba7b8a90abf93252e72cf7ce8795c923976" => :mojave
-    sha256 "4c8988d11340759b86821e64591d80aafd2834e8452ae40d69836da439008fc6" => :high_sierra
+    sha256 "0000000000000000000000000000000000000000000000000000000000000000" => :mojave
+    sha256 "0000000000000000000000000000000000000000000000000000000000000000" => :high_sierra
   end
 
   depends_on "autoconf" => :build
@@ -28,6 +28,27 @@ class Dcore < Formula
   def install
     system "cmake", ".", *std_cmake_args
     system "make", ENV["MAKEFLAGS"], "install"
+  end
+
+  plist_options :manual => "decentd"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/decentd</string>
+        <string>--daemon</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+    </dict>
+    </plist>
+  EOS
   end
 
   test do
